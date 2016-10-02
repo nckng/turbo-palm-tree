8447d619ccf26ef08905f7f806f9beb01e43108a
nckng <nng3@stuy.edu>
2016-10-02 15:38:45 -0400

    added login functionality

--- app.py
+++ app.py
@@ -1,22 +1,53 @@
 from flask import Flask, render_template, request
+import hashlib
 
 app = Flask(__name__)
 
 username = "nick"
 password = "password"
 
+
+
 @app.route("/")
-@app.route("/login/")
 def templayte():
-    print request.headers
     return render_template('newtemp.html')
 
 @app.route('/authenticate/', methods =['POST'])
 def auth():
-    if request.form['un'] == "nick" and request.form['pw'] == "password":
-        return render_template('success.html')
+    m=hashlib.md5()
+    if request.form['un'] != '' and request.form['pw']!='':
+        if request.form['input'] == 'Create Account':
+            pen = open("data/usernames.csv",'r')
+            pineapple = pen.readlines()
+            pen.close()
+            for item in pineapple:
+                if request.form['un'] == item.split(',')[0]:
+                    return render_template('newtemp.html',floo='That account name already exists!')
+            #print 'hi'
+            apple = open("data/usernames.csv",'a')
+            m.update(request.form['pw'])
+            apple.write(request.form['un']+','+m.hexdigest()+'\n')
+            apple.close()
+            return render_template('newtemp.html',floo='Account Created!')
+        else:
+            pen = open('data/usernames.csv','r')
+            applepen = pen.readlines()
+            pen.close()
+            for line in applepen:
+                un = request.form['un']
+                c = hashlib.md5()
+                c.update(request.form['pw'])
+                pw = c.hexdigest()
+                if un == line.split(',')[0]:
+                    if pw == line.split(',')[1].strip():
+                        return render_template('success.html')
+                    return render_template('newtemp.html', floo = 'Incorrect password')
+            return render_template('newtemp.html',floo='Account not found')
     else:
-        return render_template('failure.html')
+        return render_template('newtemp.html')
+
+
+
 
 if __name__ == "__main__":
     app.debug = True
